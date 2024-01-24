7a382f17ca0c238114345cbedd967d0dc7360e8b
Thiago Bitencourt <thiago.mbitencourt@gmail.com>
2024-01-24 06:48:34 -0300

    fix: clear form and disable button for no text

--- src/components/Form/Form.tsx
+++ src/components/Form/Form.tsx
@@ -1,17 +1,26 @@
+import { useState } from 'react';
 import plusIcon from '../../assets/plus-icon.svg';
 import styles from './Form.module.css';
 
 export function Form(props) {
+    const [newTodo, setNewTodo] = useState('');
 
     function handleSubmitNewTodo(event) {
         event.preventDefault();
-        props.onNewTodo("New item add");
+        if (newTodo) {
+            props.onNewTodo(newTodo);
+            setNewTodo('');
+        }
+    }
+
+    function handleNewTodoChange(event) {
+        setNewTodo(event.target.value);
     }
 
     return (
         <form className={styles['new-todo']} onSubmit={handleSubmitNewTodo}>
-            <input name="todo" placeholder="Adicione uma nova tarefa"/>
-            <button>
+            <input name="todo" placeholder="Adicione uma nova tarefa" onChange={handleNewTodoChange} value={newTodo} />
+            <button disabled={!newTodo}>
                 <span>Criar</span>
                 <img src={plusIcon} />
             </button>
